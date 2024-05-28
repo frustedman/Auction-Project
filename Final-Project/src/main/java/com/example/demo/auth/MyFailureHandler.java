c91e3bbb879ac6ed9e37b253fa9b77c25d4a652c
arnold714 <arnold714@naver.com>
2024-05-28 10:32:03 +0900

    초기설정

---
+++ Final-Project/src/main/java/com/example/demo/auth/MyFailureHandler.java
@@ -0,0 +1,24 @@
+package com.example.demo.auth;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException exception) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		String msg = exception.getMessage();
+		setDefaultFailureUrl("/error?msg=" + msg);
+		request.setAttribute("msg", "wrong id or password");
+		super.onAuthenticationFailure(request, response, exception);
+	}
+	
+}
