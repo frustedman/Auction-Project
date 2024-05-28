8ab20e8ffbfa63f9a697e2776ef7176d4db687f8
alpacaive <w6352800@naver.com>
2024-05-28 16:25:02 +0900

    Product Entity 수정, Dto, Dao, Service 구현, Controller 파일만 생성 - 이대영

--- Final-Project/src/main/java/com/example/demo/product/Product.java
+++ Final-Project/src/main/java/com/example/demo/product/Product.java
@@ -1,7 +1,6 @@
 package com.example.demo.product;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -13,6 +12,8 @@ import lombok.*;
 public class Product {
 
     @Id
+    @SequenceGenerator(name = "seq_gen", sequenceName = "seq_product", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_product")
     private int num;
 
     private String name;
@@ -21,6 +22,6 @@ public class Product {
     private String img3;
     private String img4;
     private String img5;
-    private String categories;
+    private String categories; // 의류, 디지털 기기, 골동품, 뷰티/잡화, 스포츠용품, 애완용품, 인력, 기타
 
 }
---
+++ Final-Project/src/main/java/com/example/demo/product/ProductDao.java
@@ -0,0 +1,15 @@
+package com.example.demo.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface ProductDao extends JpaRepository<Product, Integer> {
+    // 카테고리로 검색
+    ArrayList<Product> findByCategories(String Categories);
+
+    // 이름으로 검색
+    ArrayList<Product> findByNameLike(String name);
+}
