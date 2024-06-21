f16b61f2cd7f253b64109d7cb7eece1d0284b160
arnold714 <arnold714@naver.com>
2024-06-21 12:11:27 +0900

    Card FIX

--- Final-Project/src/main/java/com/example/demo/card/CardDao.java
+++ Final-Project/src/main/java/com/example/demo/card/CardDao.java
@@ -7,6 +7,6 @@ import com.example.demo.card.Card.Type;
 import java.util.Date;
 
 @Repository
-public interface CardDao extends JpaRepository<Card, Integer> {
+public interface CardDao extends JpaRepository<Card, String> {
     Card findCardByCardnumAndValidDateAndCvcAndPwdAndType(String cardnum, int validDate, int cvc, int password, Type type);
 }
